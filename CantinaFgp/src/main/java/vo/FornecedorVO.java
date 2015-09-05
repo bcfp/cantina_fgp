605f20a092be0a8080eeed861066719099e94ee4
bcfp <bcfp@486d7ec7-847a-47b8-93c3-2007f8dd9325>
2015-09-05 02:47:18 +0000

    Bruno

--- CantinaFgp/src/main/java/vo/FornecedorVO.java
+++ CantinaFgp/src/main/java/vo/FornecedorVO.java
@@ -1,5 +1,37 @@
 package vo;
 
-public class FornecedorVO {
+public class FornecedorVO extends GenericVO {
 
+	private String nome;
+	private String contato;
+	private Boolean ativo;
+	
+	public FornecedorVO() {
+
+	}
+
+	protected String getNome() {
+		return nome;
+	}
+
+	protected void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	protected String getContato() {
+		return contato;
+	}
+
+	protected void setContato(String contato) {
+		this.contato = contato;
+	}
+
+	protected Boolean getAtivo() {
+		return ativo;
+	}
+
+	protected void setAtivo(Boolean ativo) {
+		this.ativo = ativo;
+	}
+	
 }
